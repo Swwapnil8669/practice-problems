ea355d7f01b4cd45b45b7c39841a8d07e89dffb5
Swwapnil8669 <swapnilwagh0412@gmail.com>
2025-02-13 18:28:18 +0530

    Time: 1402 ms (70.39%), Space: 0B (100%) - LeetHub

---
+++ 1415-students-and-examinations/1415-students-and-examinations.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+
+select s.student_id , s.student_name , sub.subject_name ,count(ex.subject_name) as attended_exams                                 
+from Students s
+cross join Subjects sub
+left join  Examinations ex
+on s.student_id = ex.student_id
+and sub.subject_name = ex.subject_name
+group by s.student_id , s.student_name , sub.subject_name
+order by s.student_id , s.student_name

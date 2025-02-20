05a25e5a1165335e013ea9c93ffd8d7ef8077e6c
Swwapnil8669 <swapnilwagh0412@gmail.com>
2025-02-21 01:30:21 +0530

    Time: 55 ms (19.88%), Space: 55.4 MB (32.95%) - LeetHub

---
+++ 0940-fruit-into-baskets/0940-fruit-into-baskets.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int totalFruit(int[] f) {
+        Map<Integer,Integer> map = new HashMap<>();
+        int l = 0;
+        int r = 0;
+        int max = 0;
+        while(r<f.length){
+            
+            if(map.containsKey(f[r])){
+                map.put(f[r],map.get(f[r])+1);
+               
+            }else{
+                map.put(f[r],1);
+            }
+
+            
+            while(map.size()>2){
+                map.put(f[l],map.get(f[l])-1);
+                if(map.get(f[l])==0){
+                    map.remove(f[l]);   
+                }
+                l++;
+
+            }
+            
+             max = Math.max(max,r-l+1);
+              r++;
+        }
+        return max;
+    }
+}

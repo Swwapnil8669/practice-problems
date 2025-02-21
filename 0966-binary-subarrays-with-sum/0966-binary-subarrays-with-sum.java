480bd07bd6bd35b2be57f5e050cc93efc1f2dc38
Swwapnil8669 <swapnilwagh0412@gmail.com>
2025-02-21 15:23:21 +0530

    Time: 1831 ms (6.13%), Space: 48.1 MB (37.65%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.java
@@ -1,23 +1,36 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-       return coutSub(nums , goal) -  coutSub(nums , goal-1);
+int count = 0;
+for(int i = 0; i<nums.length ;i++){
+    int sum = 0;
+    for(int j = i ; j<nums.length ; j++){
+           sum +=nums[j];
+           if(sum == goal){
+            count ++;
+           }
+    }
+}
+return count ;
 
-    }
-   public int coutSub(int[] nums , int goal){
-    if(goal < 0){
-       return 0;
-    }
-    int l = 0;
-    int len = 0;
-    int sum  = 0;
-    for(int r =  0;r<nums.length ;r++){
-        sum += nums[r];
-        while(sum > goal){
-           sum = sum - nums[l];
-              l++;
-        }
-       len += r - l +1;
-    }
-    return len;
-   }
+//        return coutSub(nums , goal) -  coutSub(nums , goal-1);
+
+//     }
+//    public int coutSub(int[] nums , int goal){
+//     if(goal < 0){
+//        return 0;
+//     }
+//     int l = 0;
+//     int len = 0;
+//     int sum  = 0;
+//     for(int r =  0;r<nums.length ;r++){
+//         sum += nums[r];
+//         while(sum > goal){
+//            sum = sum - nums[l];
+//               l++;
+//         }
+//        len += r - l +1;
+//     }
+//     return len;
+//    }
+}
 }
